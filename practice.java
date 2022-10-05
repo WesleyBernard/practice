19417127b18e034a5e7428dfad8c509ded9fc03b
Wesley Bernard <wesley.bernard64@gmail.com>
2022-10-04 20:24:38 -0400

    adding "tests"

--- practice.java
+++ practice.java
@@ -6,6 +6,13 @@ public class practice {
         int[] nums = new int[] {2,7,11,15};
         int target = 9;
         System.out.println(Arrays.toString(twoSum(nums, target)));
+        nums = new int[] {3,2,4};
+        target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+        nums = new int[] {3,3};
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+
     }
 
     public static int[] twoSum(int[] nums, int target) {
